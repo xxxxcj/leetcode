58c765887162b949b06f4dec9bc6a83defff5999
wangwang <1014212816@qq.com>
2021-10-18 23:07:31 +0800

    finish leetcode 167 two sum II

--- 167(Two Sum II - Input array is sorted).py
+++ 167(Two Sum II - Input array is sorted).py
@@ -1,2 +1,9 @@
 class Solution:
     def twoSum(self, numbers: list[int], target: int) -> list[int]:
+        num_dict = {}
+        for idx, item in enumerate(numbers):
+            num_dict[item] = idx
+
+        for idx, num in enumerate(numbers):
+            if target - num in num_dict and idx != num_dict[target-num]:
+                return [idx+1, num_dict[target-num]+1]
